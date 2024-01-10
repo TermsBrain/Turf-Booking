38364101c2a01f11b060bd5409e4f0b58ba2cd06
Rafi <rafitanjibulhasan@gmail.com>
2024-01-10 15:05:46 +0600

    home page added

---
+++ home.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="assets/css/homestyle.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-VoMzfrEiEKXG9P5XpoGhiXYrQBE8dJBKmyNePqEXaLyL9Z9nUzqsKusjUy4z1pgl5RVOdPeUdqA0vF9zf2iqrDQ==" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha384-cU8e3XlqzyslKG6ZO4eyL/nBEiSNZjFfzQPpgv5vPEKzBm7lMQoqMBbqL7BfFOlM" crossorigin="anonymous">
+
+  <title>Turf Website</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light" style="background: linear-gradient(to right, #3498db, #6dd5fa);">
+    <a class="navbar-brand" href="#" style="font-family: 'Your Font', sans-serif; font-size: 24px; font-weight: bold; color: #fff;">Your Brand</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#faq">FAQ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link book-now-btn" href="booking.php">Book Now</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="cover-content">
+    <!-- Left Side (Button and Tagline) -->
+    <div class="left-content">
+      <h3 style="color:#000000">Unleash Your Inner Athlete</h3>
+      <p class="tagline">Your tagline goes here</p>
+      <a href="booking.php"><button class="cta-button">Book Now</button></a>
+      <!-- <button class="cta-button">Book Now</button> -->
+    </div>
+    <!-- Cover Photo -->
+    <img src="turf.webp" alt="Cover Photo" class="cover-photo">
+  </div>
+
+
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about section-bg">
+    <div class="container" data-aos="fade-up">
+
+      <div class="section-title">
+        <h2>About</h2>
+        <h3><span>Discover More About [Name] Turf</span></h3>
+        <p></p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+          <img src="turf-about.jpg" class="img-fluid" alt=" [Name] Turf Image">
+        </div>
+        <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
+          <h3 class="mb-4">Unleash Your Inner Athlete</h3>
+          <p>
+            Experience the perfect fusion of passion and sportsmanship at [Name] Turf – your unrivaled destination for heart-pounding Olympic-style football played on meticulously maintained turf courts. Immerse yourself in the game, book your playtime slots, and savor the thrill of competition. [Name] Turf: Where Football and Fun Collide!</p>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <section id="features" class="features section-bg">
+    <div class="container" data-aos="fade-up">
+
+      <div class="section-title">
+        <h2 class="text-center">Features & Facilities</h2>
+        <p>Explore the Exceptional Amenities at Kick OFF Football Ground</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+          <h3>Facilities</h3>
+          <ul>
+            <li>Two Separate team rooms with toilet and change rooms</li>
+            <li>Drinks available to buy</li>
+            <li>Seating arrangement in the roof</li>
+            <li>Kitchen facilities</li>
+            <li>Adequate lighting and Generator</li>
+            <li>Car Parking Facilities</li>
+            <li>Top-notch sound system with wireless microphone</li>
+            <li>CCTV Camera surveillance & Free Wifi</li>
+          </ul>
+        </div>
+
+        <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-up" data-aos-delay="100">
+          <h3>Key Features</h3>
+          <ul>
+            <li>The Grass of Kick OFF Football Ground has been manufactured by FIFA Licensee manufacturer of artificial turf in China</li>
+            <li>Dual-colored Astro turf</li>
+            <li>Excellent view from the roof for enjoying the match while having food</li>
+            <li>Excellent drainage system allowing players to play during rain</li>
+            <li>Rainwater does not clog</li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <section class="map-container">
+  <div class="section-title">
+  <h3 class="text-center"><span>Find Us On Map</span></h3>
+      </div>
+    <div class="container responsive-map" data-wow-delay="0.1s">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387.67299698642506!2d91.86598556300748!3d22.421952870828974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30ad287ed3af39d1%3A0xcb2295afdaae100d!2sOli%20Ahmed%20CNG%20station!5e0!3m2!1sen!2sbd!4v1702215951917!5m2!1sen!2sbd" style="border:1;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="container">
+      <div class="row text-center text-xs-center text-sm-left text-md-left">
+        <div class="col-xs-12 col-sm-4 col-md-4">
+          <h5>Quick links</h5>
+          <ul class="list-unstyled quick-links">
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>Home</a></li>
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>About</a></li>
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>Videos</a></li>
+          </ul>
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-4">
+          <h5>Quick links</h5>
+          <ul class="list-unstyled quick-links">
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>Home</a></li>
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>About</a></li>
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>Videos</a></li>
+          </ul>
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-4">
+          <h5>Quick links</h5>
+          <ul class="list-unstyled quick-links">
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>Home</a></li>
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>About</a></li>
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+            <li><a href=" "><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+            <li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+          <ul class="list-unstyled list-inline social text-center">
+            <li class="list-inline-item"><a href=" "><i class="fa fa-facebook"></i></a></li>
+            <li class="list-inline-item"><a href=" "><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href=" "><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href=" "><i class="fa fa-google-plus"></i></a></li>
+            <li class="list-inline-item"><a href=" " target="_blank"><i class="fa fa-envelope"></i></a></li>
+          </ul>
+        </div>
+        <hr>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+          <p><a href="https://termsbrain.com/">TermsBrain LTD.</a></p>
+          <p class="h6" style="color:black">© All right Reversed.<a class="text-green ml-2" href="#" target="_blank">TermsBrain</a></p>
+        </div>
+        <hr>
+      </div>
+    </div>
+  </section>
+  <!-- ./Footer -->
+
+  <!-- Bootstrap JS and Popper.js (Required for Bootstrap components) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://kit.fontawesome.com/your_font_awesome_kit.js" crossorigin="anonymous"></script> <!-- Replace with your Font Awesome kit URL -->
+</body>
+
+</html>
