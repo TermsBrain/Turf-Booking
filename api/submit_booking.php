d512bd9e6abb302dd2f4c9895ac6c061b7051a62
mohsin-riad <morteen1997@gmail.com>
2024-01-14 15:42:50 +0600

    api updated

--- api/submit_booking.php
+++ api/submit_booking.php
@@ -9,14 +9,10 @@ $startTime = $_POST['startTime'];
 $endTime = $_POST['endTime'];
 $admin = $_POST['admin'];
 
-<<<<<<< HEAD
 $user_insert = "INSERT INTO `customers`(`name`, `phone`) VALUES ($name, $contact)";
 $user_insert_qry = mysqli_query($conn, $str);
-if ($)
 // echo $date.'<br>'.$name.'<br>'.$contact.'<br>'.$startTime.'<br>'.$endTime;
-=======
 echo $date.'<br>'.$name.'<br>'.$contact.'<br>'.$startTime.'<br>'.$endTime.'<br>'.$admin;
->>>>>>> cad5de92eb2517f0c31a31509924d52de8986ce4
 // $str = "UPDATE booking SET status = 'completed' WHERE id = $booking_id";
 // $result = mysqli_query($conn, $str);
 
